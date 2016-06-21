d9a138eabad3b36051c37ee6ccd520b3d85a6e4a
rath-prak <rathanap@gmail.com>
2016-06-22 04:18:14 +1200

    add text to landing page

--- js/main.js
+++ js/main.js
@@ -1,8 +1,27 @@
+$(document).ready(function(){
+
 var animData = {
         container: document.getElementById('container'),
         renderer: 'svg',
-        loop: true,
-        autoplay: true,
+        loop: false,
+        autoplay: false,
         path: 'https://raw.githubusercontent.com/rath-prak/Sproutmedia-test/master/js/data.json'
     };
-    var anim = bodymovin.loadAnimation(animData);
+var anim = bodymovin.loadAnimation(animData);
+
+
+// start animation 
+
+$('.main-button').click(function(){
+	anim.play();
+	// $(this).css("color", "red")
+	$(this).css({
+		color: "#aaddd8",
+		background: "#fff"
+	})
+})
+
+
+
+
+}); 

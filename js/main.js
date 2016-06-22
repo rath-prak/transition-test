1edf422b711eccc0dfe38d76e0ddf167ffa0c42a
rath-prak <rathanap@gmail.com>
2016-06-22 12:11:51 +1200

    add new data.json

--- js/main.js
+++ js/main.js
@@ -2,26 +2,26 @@ $(document).ready(function(){
 
 var animData = {
         container: document.getElementById('container'),
-        renderer: 'svg',
+        animType: 'svg',
+        renderer: 'html',
         loop: false,
         autoplay: false,
-        path: 'https://raw.githubusercontent.com/rath-prak/Sproutmedia-test/master/js/data.json'
+        // path: 'https://raw.githubusercontent.com/rath-prak/Sproutmedia-test/master/js/data.json'
+        path: 'https://raw.githubusercontent.com/rath-prak/transition-test/master/data/data.json'
     };
 var anim = bodymovin.loadAnimation(animData);
 
 
 // start animation 
 
-$('.main-button').click(function(){
-	anim.play();
-	// $(this).css("color", "red")
-	$(this).css({
-		color: "#aaddd8",
-		background: "#fff"
+	$('.main-button').click(function(){
+		anim.play();
+		// $(this).css("color", "red")
+		$(this).css({
+			color: "#aaddd8",
+			background: "#fff"
+		})
 	})
-})
-
-
-
 
 }); 
+

d26ce31697545f0a7fc69c282906fe504221e384
rath-prak <rathanap@gmail.com>
2016-07-19 21:57:44 +1200

    change animation

--- js/main.js
+++ js/main.js
@@ -1,31 +1,44 @@
 $(document).ready(function(){
 
-var animData = {
-        container: document.getElementById('container'),
-        animType: 'svg',
-        renderer: 'html',
-        loop: false,
-        autoplay: false,
-        // path: 'https://raw.githubusercontent.com/rath-prak/Sproutmedia-test/master/js/data.json'
-        path: 'https://raw.githubusercontent.com/rath-prak/transition-test/master/data/data.json'
-    };
-var anim = bodymovin.loadAnimation(animData);
+	var animData = {
+    container: document.getElementById('container'),
+    animType: 'svg',
+    renderer: 'html',
+    loop: false,
+    autoplay: false,
+    // path: 'https://raw.githubusercontent.com/rath-prak/Sproutmedia-test/master/js/data.json'
+    path: 'https://raw.githubusercontent.com/rath-prak/transition-test/master/data/data.json'
+   };
+	var anim = bodymovin.loadAnimation(animData);
+
+		$(function(){
+			var bison = new Vivus('logo',{
+			type: 'delayed',
+			duration: 200,
+			animTimingFunction: Vivus.EASE,
+			}, fillLogo )
+
+			var fillBackground = $('#logo')
+			var tagLine = $('#tag-line')
+			var tl = new TimelineMax();
+
+			function fillLogo(){
+				tl.to(fillBackground, 2, {
+				css: {
+					fill : '#fff'
+				}, 
+				ease: Bounce.easeOut
+			}).to(tagLine, 1, {
+				css: {
+					fill : '#fff'
+				},
+				ease: Bounce.easeOut
+				})
+			}	
+		})// end of function
+
+	setTimeout(function(){ anim.play(); }, 7000);
 
 
-// start animation 
 
-	$('.main-button').click(function(){
-		anim.play();
-		// $(this).css("color", "red")
-		$(this).css({
-			color: "#aaddd8",
-			background: "#fff"
-		})
-	}) 
-
-	$("window").scroll(function(){
-    	anim.play();
-	});
-
-}); 
-
+}); // of document ready function
